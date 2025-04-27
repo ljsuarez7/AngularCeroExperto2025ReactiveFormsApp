629d52eef72e0a563658d68139edaaa0a0b8609e
JaviSrz <ljsuarez7@gmail.com>
2025-04-27 09:32:29 +0200

    Implementación de formulario dinamico y validaciones

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,20 +1,9 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 //TODO: Esto podria ser un servicio que tengamos que inyectar en los components
 
 export class FormUtils {
 
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return (
-      !! form.controls[fieldName].errors &&
-      form.controls[fieldName].touched
-    );
-  }
-
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-
-    if(!form.controls[fieldName]) return null;
-
-    const errors = form.controls[fieldName].errors ?? {};
+  static getTextError(errors: ValidationErrors){
 
     for(const key of Object.keys(errors)){
 
@@ -34,6 +23,41 @@ export class FormUtils {
     }
 
     return null;
+
+  }
+
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !! form.controls[fieldName].errors &&
+      form.controls[fieldName].touched
+    );
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+
+    if(!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number){
+
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+
+    if(formArray.controls.length === 0) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+
+    return FormUtils.getTextError(errors);
 
   }
 
