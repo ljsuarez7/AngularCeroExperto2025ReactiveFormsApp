79d2dccf5bb6e398fc1ec70b6fd28f5a488cdfcb
JaviSrz <ljsuarez7@gmail.com>
2025-05-01 14:59:52 +0200

    cargamos el selector de regiones

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  myForm = this.fb.group({
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    border: ['', [Validators.required]],
+  });
+
+}
